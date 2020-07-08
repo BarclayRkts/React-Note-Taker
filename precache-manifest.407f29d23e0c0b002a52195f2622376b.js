a9ec5e86b3ccc3a78b3313f0e3083777fd4e4b1f
DeJa <barclay.deja@gmail.com>
2020-07-08 16:46:26 -0500

    Updates

---
+++ precache-manifest.407f29d23e0c0b002a52195f2622376b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01cb927d9634fa305be703ca14a806b4",
+    "url": "/React-Note-Taker/index.html"
+  },
+  {
+    "revision": "5fc552598a0b439ebcab",
+    "url": "/React-Note-Taker/static/css/main.50aa3d97.chunk.css"
+  },
+  {
+    "revision": "ae4ec77f1c1abc212dbe",
+    "url": "/React-Note-Taker/static/js/2.2e5db257.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Note-Taker/static/js/2.2e5db257.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fc552598a0b439ebcab",
+    "url": "/React-Note-Taker/static/js/main.c9c5036e.chunk.js"
+  },
+  {
+    "revision": "11e1239fda442f9e1fce",
+    "url": "/React-Note-Taker/static/js/runtime-main.31628ea3.js"
+  }
+]);
